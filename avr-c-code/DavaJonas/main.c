21fe3ec2142f4a0497e95f51ee383d2c236c972b
Luis Henrique <luishrt@hotmail.com>
2021-02-18 19:41:46 -0300

    added pins to header

--- avr-c-code/DavaJonas/main.c
+++ avr-c-code/DavaJonas/main.c
@@ -1,9 +1,35 @@
-/*
- * DavaJonas.c
- *
- * Created: 18/02/2021 19:30:06
- * Author : luish
- */ 
+#define LINE_THRESHOLD 700
+
+// LED
+#define LED PD6
+
+// left motor
+#define pwmL PB1
+#define leftMotor1 PD7
+#define leftMotor2 PB0
+
+// right motor
+#define pwmR PD3
+#define rightMotor1 PD5
+#define rightMotor2 PD4
+
+// DIP switch
+#define DIP1 PB2
+#define DIP2 PB3
+#define DIP3 PB4
+#define DIP4 PB5
+
+// Robocore's line sensor
+#define lineL PC0
+#define lineR PC1
+
+// Jsumo's distance sensor
+#define distL PC2
+#define distR PC3
+
+// Jsumo's micro-start
+#define microST PD2
+/*******PINOUT DEFINES - END*********/
 
 #include <avr/io.h>
 
