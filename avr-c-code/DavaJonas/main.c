781bd95b26b706532986cbb78879ccf185635275
Luis Henrique <luishrt@hotmail.com>
2021-02-18 19:35:55 -0300

    added avr c project

---
+++ avr-c-code/DavaJonas/main.c
@@ -0,0 +1,18 @@
+/*
+ * DavaJonas.c
+ *
+ * Created: 18/02/2021 19:30:06
+ * Author : luish
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
