dda36534ced1e2a66bfeadd512f1ff74524f4b11
unknown <daniel.stuart@mobi7.com.br>
2021-02-19 22:37:21 -0300

    Change main project to C++ compiler + code restructuration

--- DavaJonas-avr/DavaJonas-avr/main.cpp
+++ DavaJonas-avr/DavaJonas-avr/main.cpp
@@ -1,11 +1,18 @@
 #define F_CPU 16000000UL
 
+//GCC 4+ needs theses functions for static variables
+__extension__ typedef int __guard __attribute__((mode (__DI__)));
+
+int __cxa_guard_acquire(__guard *g) {return !*(char *)(g);};
+void __cxa_guard_release (__guard *g) {*(char *)g = 1;};
+void __cxa_guard_abort (__guard *) {};
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-#define debug
-//#define noDIP
+//#define debug
+#define DIP_VAL 0b0000
 
 #ifdef debug
 #include <stdio.h>
@@ -50,16 +57,28 @@
 
 // Jsumo's micro-start
 #define microST PD2
+
+//RTDM Start States
+#define END 0
+#define CHECKALL 1
+#define CHECKLINE 2
+
 /*******PINOUT DEFINES - END*********/
 
 /*******FUNCTIONS*******/
-void decisionMaking();
 void MotorL(uint8_t, uint8_t); // left motor / motor esquerdo / motor izquierdo
 void MotorR(uint8_t, uint8_t); // right motor / motor direito / motor derecho
 uint8_t readDIP(); // read DIP switch / ler chave DIP / leer el interruptor DIP
 uint8_t lineCheck();
 uint8_t distCheck();
+void RTDM();
+uint8_t CSL();
+void FDL(uint8_t);
+void FAL(uint8_t);
+void CPL();
 /*******FUNCTIONS - END*******/
+
+uint8_t dip = 0;
 
 #ifdef debug
 #define UART_PRESCALER 8 //115200
@@ -79,10 +98,16 @@ void sendArray(char array[]) {
 
 void(* resetSoftware)(void) = 0;
 
+volatile uint16_t millis;
+
+ISR (TIMER0_COMPA_vect){
+	millis++;
+}
+
 ISR (INT0_vect) {
-#ifdef debug
+	#ifdef debug
 	log("Stopping...\n");
-#endif
+	#endif
 	MotorL(FORWARD(0));
 	MotorR(FORWARD(0));
 	PORTB &= ~(1 << LED);
@@ -93,13 +118,23 @@ ISR (INT0_vect) {
 int main(void)
 {
 	
-#ifdef debug //UART Config
+	#ifdef debug //UART Config
 	UBRR0H = (UART_PRESCALER>>8);
 	UBRR0L = (UART_PRESCALER);
 	
 	UCSR0C = 0x06;       /* Set frame format: 8data, 1stop bit  */
 	UCSR0B = (1<<TXEN0); /* Enable  transmitter                 */
-#endif
+	#endif
+
+	//Set millis timer
+	millis = 0;
+	TCCR0A = 0; // set entire TCCR0A register to 0
+	TCCR0B = 0; // same for TCCR0B
+	TCNT0  = 0; // initialize counter value to 0
+	TCCR0A |= (1 << WGM01);
+	TCCR0B |= (0 << CS02) | (1 << CS01) | (1 << CS00);
+	TIMSK0 |= (1 << OCIE0A);
+	OCR0A = 249;
 	
 	/****************PINOUT CONFIG****************/
 	// OUTPUTS
@@ -146,259 +181,101 @@ int main(void)
 	log("Ready!\n");
 	
 	while (!(PIND & (1 << microST))) {};
-		
-	log("Starting...\n");		
-	uint8_t dip = readDIP();
-	switch (dip) {
-		case 0x0: //Frente + Busca até a linha -> A3
-		while(1){
-			decisionMaking();
-			MotorL(FORWARD(200));
-			MotorR(FORWARD(200));
-		}
-		break;
-		case 0x01: //Curva direita fechada + Busca até a linha -> B5
-		for(uint16_t i = 0; i <=2000; i++){
-			MotorL(FORWARD(255));
-			MotorR(FORWARD(140));
-			//decisionMaking();
-			_delay_ms(1);
-		}
-		while(1){
-			decisionMaking();
-			MotorL(FORWARD(200));
-			MotorR(FORWARD(200));
-		}
-		break;
-		case 0x02: //Curva direita aberta + Busca até a linha
-		for(int i = 0; i <=1000; i++){
-			MotorL(FORWARD(127));
-			MotorR(FORWARD(75));
-			decisionMaking();
-			_delay_ms(1);
-		}
-		while(1){
-			decisionMaking();
-			MotorL(FORWARD(200));
-			MotorR(FORWARD(200));
-		}
-		break;
-		case 0x03: //Curva esquerda fechada + Busca até a linha
-		for(int i = 0; i <=1000; i++){
-			MotorL(FORWARD(70));
-			MotorR(FORWARD(127));
-			decisionMaking();
-			_delay_ms(1);
-		}
-		while(1){
-			decisionMaking();
-			MotorL(FORWARD(200));
-			MotorR(FORWARD(200));
-		}
-		break;
-		case 0x04: //Curva esquerda aberta + Busca até a linha
-		for(int i = 0; i <=1000; i++){
-			MotorL(FORWARD(75));
-			MotorR(FORWARD(127));
-			decisionMaking();
-			_delay_ms(1);
-		}
-		while(1){
-			decisionMaking();
-			MotorL(FORWARD(200));
-			MotorR(FORWARD(200));
-		}
-		break;
-		case 0x05: //Desvio 45º Direito + Busca até a linha
-		for(int i = 0; i <=1000; i++){
-			MotorL(FORWARD(200));
-			MotorR(FORWARD(180));
-			decisionMaking();
-			_delay_ms(1);
-		}
-		decisionMaking();
-		while(1){
-			decisionMaking();
-			MotorL(FORWARD(200));
-			MotorR(FORWARD(200));
-		}
-		break;
-		case 0x06: //Desvio 45º Esquerdo + Busca até a linha
-		for(int i = 0; i <=1000; i++){
-			MotorL(FORWARD(180));
-			MotorR(FORWARD(200));
-			decisionMaking();
-			_delay_ms(1);
-		}
-		while(1){
-			decisionMaking();
-			MotorL(FORWARD(200));
-			MotorR(FORWARD(200));
-		}
-		break;
-		case 0x07: //Parado 3s + Busca até a linha
-		for(int i = 0; i <= 3000; i++){
-			decisionMaking();
-			_delay_ms(1);
-		}
-		while (1) {
-			decisionMaking();
-			MotorL(FORWARD(200));
-			MotorR(FORWARD(200));
-		}
-		break;
-		case 0x08: //Frente + Busca lenta alternada
-		MotorL(FORWARD(200));
-		MotorR(FORWARD(200));
-		while(1){
-			uint8_t left = 1;
-			for(int i = 0; i <=500; i++){
-				decisionMaking();
-				MotorL(FORWARD(180 + left ? 20 : 0));
-				MotorR(FORWARD(200 - left ? 20 : 0));
-				_delay_ms(1);
-				if(i == 500) left = !left;
-			}
-		}
-		break;
-		case 0x09: //Curva direita fechada + Busca lenta alternada
-		for(int i = 0; i <=1000; i++){
-			MotorL(FORWARD(127));
-			MotorR(FORWARD(70));
-			decisionMaking();
-			_delay_ms(1);
-		}
-		while(1){
-			uint8_t left = 1;
-			for(int i = 0; i <=500; i++){
-				decisionMaking();
-				MotorL(FORWARD(180 + left ? 20 : 0));
-				MotorR(FORWARD(200 - left ? 20 : 0));
-				_delay_ms(1);
-				if(i == 500) left = !left;
-			}
-		}
-		break;
-		case 0x0A: //Curva direita aberta + Busca lenta alternada
-		for(int i = 0; i <=1000; i++){
-			MotorL(FORWARD(127));
-			MotorR(FORWARD(75));
-			decisionMaking();
-			_delay_ms(1);
-		}
-		while(1){
-			uint8_t left = 1;
-			for(int i = 0; i <=500; i++){
-				decisionMaking();
-				MotorL(FORWARD(180 + left ? 20 : 0));
-				MotorR(FORWARD(200 - left ? 20 : 0));
-				_delay_ms(1);
-				if(i == 500) left = !left;
-			}
-		}
-		break;
-		case 0x0B: //Curva esquerda fechada + Busca lenta alternada
-		for(int i = 0; i <=1000; i++){
-			MotorL(FORWARD(70));
-			MotorR(FORWARD(127));
-			decisionMaking();
-			_delay_ms(1);
-		}
-		while(1){
-			uint8_t left = 1;
-			for(int i = 0; i <=500; i++){
-				decisionMaking();
-				MotorL(FORWARD(180 + left ? 20 : 0));
-				MotorR(FORWARD(200 - left ? 20 : 0));
-				_delay_ms(1);
-				if(i == 500) left = !left;
-			}
-		}
-		break;
-		case 0x0C: //Curva esquerda aberta + Busca lenta alternada
-		for(int i = 0; i <=1000; i++){
-			MotorL(FORWARD(75));
-			MotorR(FORWARD(127));
-			decisionMaking();
-			_delay_ms(1);
-		}
-		while(1){
-			uint8_t left = 1;
-			for(int i = 0; i <=500; i++){
-				decisionMaking();
-				MotorL(FORWARD(180 + left ? 20 : 0));
-				MotorR(FORWARD(200 - left ? 20 : 0));
-				_delay_ms(1);
-				if(i == 500) left = !left;
-			}
-		}
-		break;
-		case 0x0D: //Desvio 45º Direito + Busca lenta alternada
-		MotorL(FORWARD(200));
-		MotorR(FORWARD(180));
-		decisionMaking();
-		while(1){
-			uint8_t left = 1;
-			for(int i = 0; i <=500; i++){
-				decisionMaking();
-				MotorL(FORWARD(180 + left ? 20 : 0));
-				MotorR(FORWARD(200 - left ? 20 : 0));
-				_delay_ms(1);
-				if(i == 500) left = !left;
-			}
-		}
-		break;
-		case 0x0E: //Desvio 45º Esquerdo + Busca lenta alternada
-		MotorL(FORWARD(180));
-		MotorR(FORWARD(200));
-		decisionMaking();
-		while(1){
-			uint8_t left = 1;
-			for(int i = 0; i <=500; i++){
-				decisionMaking();
-				MotorL(FORWARD(180 + left ? 20 : 0));
-				MotorR(FORWARD(200 - left ? 20 : 0));
-				_delay_ms(1);
-				if(i == 500) left = !left;
-			}
-		}
-		break;
-		case 0x0F: //Parado 3s + Busca até a linha
-		for(int i = 0; i <= 3000; i++){
-			decisionMaking();
-			_delay_ms(1);
-		}
-		while(1){
-			uint8_t left = 1;
-			for(int i = 0; i <=500; i++){
-				decisionMaking();
-				MotorL(FORWARD(180 + left ? 20 : 0));
-				MotorR(FORWARD(200 - left ? 20 : 0));
-				_delay_ms(1);
-				if(i == 500) left = !left;
-			}
-		}
-		break;
-	}
-	MotorL(STOP);
-	MotorR(STOP);
-	PORTB &= ~(1 << LED);
+	log("Starting...\n");
+	
+	#ifdef DIP_VAL
+	dip = DIP_VAL;
+	#else
+	dip = readDIP();
+	#endif
+	
+	RTDM();
+	
+	while(1) {}; //It should never get here
 }
 
 //RTDM()
-void decisionMaking(){
-	//DEFENSE
-	uint8_t lineSensor = lineCheck();
-	switch (lineSensor) {
+void RTDM() {
+	uint8_t startState = CHECKALL;
+	while (startState) {
+		startState = CSL(); //Configurable Start Logic
+		
+		//Check line sensors
+		uint8_t lineSensor = lineCheck();
+		if (lineSensor) { //If any sensor is triggered
+			FDL(lineSensor); //Fixed Defense Logic
+			continue; //Skip everything else
+		}
+		
+		if (startState == CHECKALL)	{
+			//Check distance sensors
+			uint8_t distSensor = distCheck();
+			if (distSensor) { //If any sensor is triggered
+				FAL(distSensor); //Fixed Attack Logic
+				continue; //Skip everything else
+			}
+		}
+		#ifdef debug
+		_delay_ms(1000);
+		log("\n");
+		#endif
+	}
+	
+	while (1) {
+		//Check line sensors
+		uint8_t lineSensor = lineCheck();
+		if (lineSensor) { //If any sensor is triggered
+			FDL(lineSensor); //Fixed Defense Logic
+			continue; //Skip everything else
+		}
+		
+		//Check distance sensors
+		uint8_t distSensor = distCheck();
+		if (distSensor) { //If any sensor is triggered
+			FAL(distSensor); //Fixed Attack Logic
+			continue; //Skip everything else
+		}
+		
+		CPL(); //Configurable Patrol Logic
+		
+		#ifdef debug
+		_delay_ms(1000);
+		log("\n");
+		#endif
+	}
+}
+
+//CANNOT BE BLOCKING (EX: DON'T USE SLEEP)!
+uint8_t CSL() {
+	//TODO> Add all start logics
+	static uint16_t start = millis;
+	MotorL(BACKWARD(60));
+	MotorR(FORWARD(60));
+	if (millis - start <= 2000)
+		return CHECKLINE;
+	
+	return END;
+}
+
+//CANNOT BE BLOCKING (EX: DON'T USE SLEEP)!
+void CPL() {
+	//TODO: Add all patrol logics
+	MotorL(FORWARD(60));
+	MotorR(FORWARD(60));
+}
+
+void FDL(uint8_t sensors) {
+	//TODO: change this to something more elegant (Ex: rotate until sensor returns false)
+	switch (sensors) {
 		case 0b01: //right detect
 		MotorL(BACKWARD(255));
 		MotorR(FORWARD(255));
-		_delay_ms(100);
+		_delay_ms(120);
 		break;
 		case 0b10:  //left detect
 		MotorL(FORWARD(255));
 		MotorR(BACKWARD(255));
-		_delay_ms(100);
+		_delay_ms(120);
 		break;
 		case 0b11: //frontal detect
 		MotorL(BACKWARD(255));
@@ -409,11 +286,11 @@ void decisionMaking(){
 		_delay_ms(200);
 		break;
 	}
-	//ATTACK
-	uint8_t distSensor;
+}
+
+void FAL(uint8_t sensors) {
 	do {
-		distSensor = distCheck();
-		switch (distSensor) {
+		switch (sensors) {
 			case 0b01: //right detect
 			MotorL(FORWARD(200));
 			MotorR(BACKWARD(200));
@@ -427,28 +304,22 @@ void decisionMaking(){
 			MotorR(FORWARD(255));
 			break;
 		}
-		#ifdef debug
-		_delay_ms(1000);
-		#endif
-	} while (distSensor);
-	
-	#ifdef debug
-	_delay_ms(1000);
-	#endif
+		sensors = distCheck();
+	} while (sensors);
 }
 
 uint8_t distCheck() {
 	uint8_t ret = 0;
 	if (readDist(distR))
-		ret |= 0b01;
+	ret |= 0b01;
 	if (readDist(distL))
-		ret |= 0b10;
+	ret |= 0b10;
 	
-#ifdef debug
+	#ifdef debug
 	char buf[50];
 	sprintf(buf, "distance sensor: %d\n", ret);
 	log(buf);
-#endif
+	#endif
 	
 	return ret;
 }
@@ -468,21 +339,21 @@ uint8_t lineCheck() {
 
 	uint8_t ret = 0;
 	if (sensor_r <= LINE_THRESHOLD)
-		ret |= 0b01;
+	ret |= 0b01;
 	if (sensor_l <= LINE_THRESHOLD)
-		ret |= 0b10;
-#ifdef debug
+	ret |= 0b10;
+	#ifdef debug
 	char buf[50];
 	sprintf(buf, "line sensor: %d\n", ret);
 	log(buf);
-#endif
+	#endif
 	return ret;
 }
 
 void setPWML(uint8_t pwm) {
 	if (pwm) {
 		DDRB |= (1 << pwmL);
-	} else {
+		} else {
 		DDRB &= ~(1 << pwmL);
 	}
 	OCR1A = pwm;
@@ -498,37 +369,37 @@ void MotorL(uint8_t pwm, uint8_t state){
 	// leftMotor1=0 and leftMotor2=1 -> moves forward / avanca / avanzar
 	// leftMotor1=1 and leftMotor2=0 -> moves back / recua / retrocede
 	// leftMotor1=1 and leftMotor2=1 -> stopped (braked) / parado (travado) / parado (frenado)
-#ifdef debug
+	#ifdef debug
 	char buf[50];
 	sprintf(buf, "left motor pwm: %d\n", pwm);
 	log(buf);
 	sprintf(buf, "left motor state: %d\n", state);
 	log(buf);
-#else
+	#else
 	switch (state) {
 		case 0x00:
-			PORTD |= (1 << leftMotor1);
-			PORTB &= ~(1 << leftMotor2);
-			setPWML(pwm);
-			break;
+		PORTD |= (1 << leftMotor1);
+		PORTB &= ~(1 << leftMotor2);
+		setPWML(pwm);
+		break;
 		case 0x01:
-			PORTD &= ~(1 << leftMotor1);
-			PORTB |= (1 << leftMotor2);
-			setPWML(pwm);
-			break;
+		PORTD &= ~(1 << leftMotor1);
+		PORTB |= (1 << leftMotor2);
+		setPWML(pwm);
+		break;
 		case 0x02:
-			setPWML(0);
-			PORTD |= ~(1 << leftMotor1);
-			PORTB |= (1 << leftMotor2);
-			break;
+		setPWML(0);
+		PORTD |= ~(1 << leftMotor1);
+		PORTB |= (1 << leftMotor2);
+		break;
 	}
-#endif
+	#endif
 }
 
 void setPWMR(uint8_t pwm) {
 	if (pwm) {
 		DDRD |= (1 << pwmR);
-	} else {
+		} else {
 		DDRD &= ~(1 << pwmR);
 	}
 	OCR2B = pwm;
@@ -544,31 +415,31 @@ void MotorR(uint8_t pwm, uint8_t state){
 	// leftMotor1=0 and leftMotor2=1 -> moves forward / avanca / avanzar
 	// leftMotor1=1 and leftMotor2=0 -> moves back / recua / retrocede
 	// leftMotor1=1 and leftMotor2=1 -> stopped (braked) / parado (travado) / parado (frenado)
-#ifdef debug
+	#ifdef debug
 	char buf[50];
 	sprintf(buf, "left motor pwm: %d\n", pwm);
 	log(buf);
 	sprintf(buf, "left motor state: %d\n", state);
 	log(buf);
-#else
+	#else
 	switch (state) {
 		case 0x00:
-			PORTD |= (1 << rightMotor1);
-			PORTD &= ~(1 << rightMotor2);
-			setPWMR(pwm);
-			break;
+		PORTD |= (1 << rightMotor1);
+		PORTD &= ~(1 << rightMotor2);
+		setPWMR(pwm);
+		break;
 		case 0x01:
-			PORTD &= ~(1 << rightMotor1);
-			PORTD |= (1 << rightMotor2);
-			setPWMR(pwm);
-			break;
+		PORTD &= ~(1 << rightMotor1);
+		PORTD |= (1 << rightMotor2);
+		setPWMR(pwm);
+		break;
 		case 0x02:
-			setPWMR(0);
-			PORTD |= ~(1 << rightMotor1);
-			PORTD |= (1 << rightMotor2);
-			break;
+		setPWMR(0);
+		PORTD |= ~(1 << rightMotor1);
+		PORTD |= (1 << rightMotor2);
+		break;
 	}
-#endif
+	#endif
 }
 
 /** read DIP switch / ler chave DIP / leer el interruptor DIP **/
@@ -578,19 +449,19 @@ void MotorR(uint8_t pwm, uint8_t state){
 uint8_t readDIP(){
 	uint8_t ret = 0;
 	if (PINB & (1 << DIP1))
-		ret |= 0b1000;
+	ret |= 0b1000;
 	if (PINB & (1 << DIP2))
-		ret |= 0b0100;
+	ret |= 0b0100;
 	if (PINB & (1 << DIP3))
-		ret |= 0b0010;
+	ret |= 0b0010;
 	if (PINB & (1 << DIP4))
-		ret |= 0b0001;
+	ret |= 0b0001;
 	
-#ifdef debug
+	#ifdef debug
 	char buf[50];
 	sprintf(buf, "dip switch: %d\n", ret);
 	log(buf);
-#endif
+	#endif
 
 	return ret;
 }
