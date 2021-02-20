af66e03d0a7e4ff53ea3ff1a53c0d015c4c93040
unknown <daniel.stuart@mobi7.com.br>
2021-02-19 21:20:00 -0300

    Add remote start code

--- presenceEmulator/presenceEmulator.ino
+++ presenceEmulator/presenceEmulator.ino
@@ -5,12 +5,22 @@
 //#define debug
 #define MAXDIST 400 // max distance in milimeters
 
+#define IR_RECEIVE_PIN  10
+
+#define MARK_EXCESS_MICROS    20 // recommended for the cheap VS1838 modules
+
+#include <IRremote.h>
+
 TwoWire myWire(&sercom4, 6, 7);
 
 VL53L0X sensor_r;
 VL53L0X sensor_l;
 
 void setup() {
+  REG_PORT_DIRSET0 |= PORT_PA07 | PORT_PA05 | PORT_PA11 | PORT_PA17;
+  REG_PORT_OUTCLR0 = PORT_PA11;
+  REG_PORT_OUTSET0 = PORT_PA17;
+  
 #ifdef debug
   SerialUSB.begin(115200);
 #endif
@@ -19,8 +29,6 @@ void setup() {
   myWire.begin();
   pinPeripheral(6,PIO_SERCOM_ALT);
   pinPeripheral(7,PIO_SERCOM_ALT);
-
-  REG_PORT_DIRSET0 |= (PORT_PA04 | PORT_PA10);
 
   sensor_l.setBus(&myWire);
   sensor_r.setTimeout(50);
@@ -35,29 +43,56 @@ void setup() {
     }
   }
 
+  IrReceiver.begin(IR_RECEIVE_PIN);
+
   sensor_r.startContinuous();
   sensor_l.startContinuous();
 }
 
 void loop() {
+  if (IrReceiver.decode()) {
+#ifdef debug
+    Serial.println("IR Receiver:");
+    Serial.print(F("  Address: 0x"));
+    Serial.println(IrReceiver.decodedIRData.address, HEX);
+    Serial.print(F("  Command: 0x"));
+    Serial.println(IrReceiver.decodedIRData.command , HEX);
+#endif
+    if (IrReceiver.decodedIRData.address == 0x707) {
+      switch (IrReceiver.decodedIRData.command) {
+        case 0x1:
+          REG_PORT_OUTSET0 = PORT_PA17;
+          REG_PORT_OUTCLR0 = PORT_PA11;
+          break;
+        case 0x2:
+          REG_PORT_OUTCLR0 = PORT_PA17;
+          REG_PORT_OUTSET0 = PORT_PA11;
+          break;
+      }
+    }
+
+    IrReceiver.resume();
+  }
+  
   uint16_t dist_l, dist_r;
   dist_r = sensor_r.readRangeContinuousMillimeters();
   dist_l = sensor_l.readRangeContinuousMillimeters();
 
   if (dist_r <= MAXDIST) {
-    REG_PORT_OUTSET0 = PORT_PA04;
+    REG_PORT_OUTSET0 = PORT_PA05;
   } else {
-    REG_PORT_OUTCLR0 = PORT_PA04;
+    REG_PORT_OUTCLR0 = PORT_PA05;
   }
   
   if (dist_l <= MAXDIST) {
-    REG_PORT_OUTSET0 = PORT_PA10;
+    REG_PORT_OUTSET0 = PORT_PA07;
   } else {
-    REG_PORT_OUTCLR0 = PORT_PA10;
+    REG_PORT_OUTCLR0 = PORT_PA07;
   }
 
 #ifdef debug
-  SerialUSB.print("right: ");
+  SerialUSB.println("Distance Sensors:");
+  SerialUSB.print("  right: ");
   SerialUSB.print(dist_r <= MAXDIST);
   SerialUSB.print(" - ");
   SerialUSB.print(dist_r);
